f9e82390a44296184de2510541d4d9ca638d114d
Mathias <maticrocco330@gmail.com>
2025-07-23 11:48:17 -0300

    sbo landing

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", function () {
+    const animado = document.querySelector(".mensaje-animado");
+    if (animado) {
+      animado.classList.add("visible");
+    }
+  });
+  
